9eb52ea469c23acdde2c2695f79f62fb2025fc69
Erwan Marega <maregaerwan@gmail.com>
2026-01-27 12:55:48 +0100

    feat(cli): add learn command for analyzing repository commit history

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
 import "dotenv/config";
 import { Command } from "commander";
 import { commitCommand } from "./cli/commit";
 import { initCommand } from "./cli/init";
+import { learnCommand } from "./cli/learn";
 
 const program = new Command();
 
@@ -19,5 +21,19 @@ program
   .command("commit")
   .description("Interactive commit with AI assistance")
   .action(commitCommand);
+
+program
+  .command("learn")
+  .description("Learn commit style from repository history")
+  .option("-c, --commits <number>", "Number of commits to analyze", "100")
+  .option("-a, --author <email>", "Analyze commits from specific author")
+  .option("-r, --reset", "Reset learned style")
+  .action((options) => {
+    learnCommand({
+      commits: parseInt(options.commits),
+      author: options.author,
+      reset: options.reset
+    });
+  });
 
 program.parse();
